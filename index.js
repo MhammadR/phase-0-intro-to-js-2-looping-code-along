4b9a18d44f293e8632126516ad1ce3dc7339e306
MhammadR <114111412+MhammadR@users.noreply.github.com>
2023-01-19 15:35:18 +0200

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-// Code your solutions in this file
+const sinon = "sinon";
+
+function writeCards(arr, word) {
+  const ar = [];
+  for (let i = 0; i < arr.length; i++) {
+    ar.push(`Thank you, ${arr[i]}, for the wonderful ${word} gift!`);
+  }
+  return ar;
+}
+
+function countDown(num) {
+  let i = num;
+  while (i >= 0) {
+    console.log(i);
+    i--;
+  }
+}
+
